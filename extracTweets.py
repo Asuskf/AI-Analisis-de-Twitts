f05da2da4d38631fbbbf3bbf191c982e5b2bd408
David <david_k2401@hotmail.com>
2019-06-12 12:37:51 -0500

    update script extract tweet

--- extracTweets.py
+++ extracTweets.py
@@ -1,25 +1,55 @@
+from pymongo.mongo_client import MongoClient
 from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler, Stream
+from tweepy import OAuthHandler, Stream, cursor, API
 
 import twitterCredentials
+import pandas as pd
+import json
+
+
+class TwitterAuthenticator:
+    @staticmethod
+    def authenticate_twitter_app():
+        authentication = OAuthHandler(twitterCredentials.consumerKey, twitterCredentials.consumerSecret)
+        authentication.set_access_token(twitterCredentials.accessToken, twitterCredentials.accessTokenSecret)
+        return authentication
+
+
+class ExtractTweets():
+    @staticmethod
+    def stream_tweets(list_words):
+        authentication = TwitterAuthenticator.authenticate_twitter_app()
+        listener = TweetListener()
+        stream = Stream(authentication, listener)
+        stream.filter(track=list_words, locations=[-92.2072, -5.0159, -75.1925, 1.8836], languages=['es'])
 
 
 class TweetListener(StreamListener):
 
     def on_data(self, raw_data):
-        print(raw_data)
-        return True
+        try:
+            data = pd.read_json(raw_data)
+            client = MongoClient('localhost', 27017)
+            data_base = client['DB_DataSourceTweets']
+            collection = data_base['Doc_Tweets']
+            jsonData = json.loads(data.T.to_json()).values()
+            new_tweet = collection.insert(jsonData)
+            print(new_tweet)
+            return True
+
+        except BaseException as e:
+            print("Error en on_data: %s" % str(e))
 
     def on_error(self, status_code):
+        if status_code == 420:
+            return False
         print(status_code)
 
 
 if __name__ == "__main__":
 
-    listener = TweetListener()
-    authentication = OAuthHandler(twitterCredentials.consumerKey, twitterCredentials.consumerSecret)
-    authentication.set_access_token(twitterCredentials.accessToken, twitterCredentials.accessTokenSecret)
+    listWords = ['huawei', 'apple']
+    tweets = ExtractTweets()
+    tweets.stream_tweets(listWords)
 
-    stream = Stream(authentication, listener)
 
-    stream.filter(track=['huawei', 'apple'], locations=[-92.2072, -5.0159, -75.1925, 1.8836], languages=['es'])
